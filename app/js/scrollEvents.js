a4a8516db6324d4e131280e28aa193086bdf3758
Nicklas Bergmann <nicklasbergmann@Nicklass-MacBook-Pro.local>
2017-09-19 14:53:18 +0200

    subnav workinprogress

--- app/js/scrollEvents.js
+++ app/js/scrollEvents.js
@@ -31,3 +31,12 @@ $(function() {
 
 
 });
+var prev = 0;
+var $window = $(window);
+var nav = $('.subnavbar');
+
+$window.on('scroll', function(){
+  var scrollTop = $window.scrollTop();
+  nav.toggleClass('hidden', scrollTop > prev);
+  prev = scrollTop;
+});
