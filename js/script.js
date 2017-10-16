ab1d790b680a6becb37f99de76c2a3b8b50072c6
Afshin Moatari <Afshinmoatari@user.noreply.github.com>
2017-10-16 11:15:49 +0200

    Update 2017-10-16T09:15:30.313Z

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// $(function() {
-//
-//   $('.checkbox > a').click(function() {
-//     $('.checkbox > a').not(this).each(function() {
-//       $(this).parent().slideUp();
-//     });
-//   })
-//
-// });
