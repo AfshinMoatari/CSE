4862c2c393af442e9fb4ceb0e88e6c8242b864bb
Afshin Moatari <Afshinmoatari@user.noreply.github.com>
2017-10-24 12:18:11 +0200

    Update 2017-10-24T10:18:09.458Z

--- js/fullPage.js
+++ js/fullPage.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   $('.fullpage').fullpage({
-    scrollBar: true,
+    // scrollBar: true,
     verticalCentered: true,
     paddingTop: '135px',
     anchors: ['first', 'second', 'third', 'fourth', 'fifth', 'sixth'],
     menu: '#subnav',
     // scrollOverflow: true
   });
+
 });
