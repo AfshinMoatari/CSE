2970bf19778df47dfd0d125a84db03df7c00b295
Afshin Moatari <Afshinmoatari@user.noreply.github.com>
2017-11-17 06:29:06 +0100

    Improved the programs page animation for Safari 11.0.1

--- app/js/SVGanimation.js
+++ app/js/SVGanimation.js
@@ -1,6 +1,6 @@
 $(function() {
   var cbsHero = document.getElementById("cbs-hero");
-  var cseProofPlan = document.getElementById("cse-proof-plan");
+  // var cseProofPlan = document.getElementById("cse-proof-plan");
   if (cbsHero) {
     cbsHero.addEventListener("load", function() {
       var doc = this.getSVGDocument();
@@ -33,90 +33,90 @@ $(function() {
       tween.play()
     });
   }
-  if (cseProofPlan) {
-    cseProofPlan.addEventListener("load", function() {
-      var doc = this.getSVGDocument();
-      var lines = doc.getElementsByClassName("line");
-      var clouds = doc.getElementsByClassName("cloud");
-      var bubbles = doc.getElementsByClassName("bubble");
-      var texts = doc.getElementsByClassName("text");
-
-      // build a tween
-      tween = new TimelineMax();
-      var tween = tween.to(lines[4], .1, {
-        css: {
-          className: '+=active',
-          animationDuration: '5s'
-        }
-      }, 0).to(bubbles[4], .1, {
-        css: {
-          opacity: 1,
-          className: '+=active',
-          animationDuration: '.5s'
-        }
-      }, 0.5).from(texts[4], 1, {
-        autoAlpha: 0,
-        x: '-=40'
-      }, 0.5).to(lines[3], .1, {
-        css: {
-          className: '+=active',
-          animationDuration: '3.5s'
-        }
-      }, 1).to(bubbles[3], .1, {
-        css: {
-          opacity: 1,
-          className: '+=active',
-          animationDuration: '.5s'
-        }
-      }, 2).from(texts[3], 1, {
-        autoAlpha: 0,
-        x: '+=40'
-      }, 2).to(lines[2], .1, {
-        css: {
-          className: '+=active',
-          animationDuration: '5s'
-        }
-      }, 2).to(bubbles[2], .1, {
-        css: {
-          opacity: 1,
-          className: '+=active',
-          animationDuration: '.5s'
-        }
-      }, 2.7).from(texts[2], 1, {
-        autoAlpha: 0,
-        x: '+=40'
-      }, 2.7).to(lines[1], .1, {
-        css: {
-          className: '+=active',
-          animationDuration: '5s'
-        }
-      }, 3).to(bubbles[1], .1, {
-        css: {
-          opacity: 1,
-          className: '+=active',
-          animationDuration: '.5s'
-        }
-      }, 3.7).from(texts[1], 1, {
-        autoAlpha: 0,
-        x: '+=40'
-      }, 3.7).to(lines[0], .1, {
-        css: {
-          className: '+=active',
-          animationDuration: '5s'
-        }
-      }, 3).to(bubbles[0], .1, {
-        css: {
-          opacity: 1,
-          className: '+=active',
-          animationDuration: '.5s'
-        }
-      }, 4.4).from(texts[0], 1, {
-        autoAlpha: 0,
-        x: '-=40'
-      }, 4.4);
-      tween.play()
-    });
-  }
+  // if (cseProofPlan) {
+  //   cseProofPlan.addEventListener("load", function() {
+  //     var doc = this.getSVGDocument();
+  //     var lines = doc.getElementsByClassName("line");
+  //     var clouds = doc.getElementsByClassName("cloud");
+  //     var bubbles = doc.getElementsByClassName("bubble");
+  //     var texts = doc.getElementsByClassName("text");
+  //
+  //     // build a tween
+  //     tween = new TimelineMax();
+  //     var tween = tween.to(lines[4], .1, {
+  //       css: {
+  //         className: '+=active',
+  //         animationDuration: '5s'
+  //       }
+  //     }, 0).to(bubbles[4], .1, {
+  //       css: {
+  //         opacity: 1,
+  //         className: '+=active',
+  //         animationDuration: '.5s'
+  //       }
+  //     }, 0.5).from(texts[4], 1, {
+  //       autoAlpha: 0,
+  //       x: '-=40'
+  //     }, 0.5).to(lines[3], .1, {
+  //       css: {
+  //         className: '+=active',
+  //         animationDuration: '3.5s'
+  //       }
+  //     }, 1).to(bubbles[3], .1, {
+  //       css: {
+  //         opacity: 1,
+  //         className: '+=active',
+  //         animationDuration: '.5s'
+  //       }
+  //     }, 2).from(texts[3], 1, {
+  //       autoAlpha: 0,
+  //       x: '+=40'
+  //     }, 2).to(lines[2], .1, {
+  //       css: {
+  //         className: '+=active',
+  //         animationDuration: '5s'
+  //       }
+  //     }, 2).to(bubbles[2], .1, {
+  //       css: {
+  //         opacity: 1,
+  //         className: '+=active',
+  //         animationDuration: '.5s'
+  //       }
+  //     }, 2.7).from(texts[2], 1, {
+  //       autoAlpha: 0,
+  //       x: '+=40'
+  //     }, 2.7).to(lines[1], .1, {
+  //       css: {
+  //         className: '+=active',
+  //         animationDuration: '5s'
+  //       }
+  //     }, 3).to(bubbles[1], .1, {
+  //       css: {
+  //         opacity: 1,
+  //         className: '+=active',
+  //         animationDuration: '.5s'
+  //       }
+  //     }, 3.7).from(texts[1], 1, {
+  //       autoAlpha: 0,
+  //       x: '+=40'
+  //     }, 3.7).to(lines[0], .1, {
+  //       css: {
+  //         className: '+=active',
+  //         animationDuration: '5s'
+  //       }
+  //     }, 3).to(bubbles[0], .1, {
+  //       css: {
+  //         opacity: 1,
+  //         className: '+=active',
+  //         animationDuration: '.5s'
+  //       }
+  //     }, 4.4).from(texts[0], 1, {
+  //       autoAlpha: 0,
+  //       x: '-=40'
+  //     }, 4.4);
+  //     tween.play()
+  //   });
+  // }
   $('.social .item').each(function() {
 
     function over() {
@@ -129,5 +129,88 @@ $(function() {
     // call a hover scene
     $(this).hover(over, out);
   });
+  $('.cse-proof-plan').each(function() {
+    var lines = $(this).find('.line');
+    var bubbles = $(this).find('.bubble');
+    var texts = $(this).find('.text');
+    console.log(lines[4]);
+    // build a tween
+    tween = new TimelineMax();
+    var tween = tween.to(lines[4], .1, {
+      css: {
+        className: '+=active',
+        animationDuration: '5s'
+      }
+    }, 0).to(bubbles[4], .1, {
+      css: {
+        opacity: 1,
+        className: '+=active',
+        animationDuration: '.5s'
+      }
+    }, 0.5).from(texts[4], 1, {
+      autoAlpha: 0,
+      x: '-=40'
+    }, 0.5).to(lines[3], .1, {
+      css: {
+        className: '+=active',
+        animationDuration: '3.5s'
+      }
+    }, 1).to(bubbles[3], .1, {
+      css: {
+        opacity: 1,
+        className: '+=active',
+        animationDuration: '.5s'
+      }
+    }, 2).from(texts[3], 1, {
+      autoAlpha: 0,
+      x: '+=40'
+    }, 2).to(lines[2], .1, {
+      css: {
+        className: '+=active',
+        animationDuration: '5s'
+      }
+    }, 2).to(bubbles[2], .1, {
+      css: {
+        opacity: 1,
+        className: '+=active',
+        animationDuration: '.5s'
+      }
+    }, 2.7).from(texts[2], 1, {
+      autoAlpha: 0,
+      x: '+=40'
+    }, 2.7).to(lines[1], .1, {
+      css: {
+        className: '+=active',
+        animationDuration: '5s'
+      }
+    }, 3).to(bubbles[1], .1, {
+      css: {
+        opacity: 1,
+        className: '+=active',
+        animationDuration: '.5s'
+      }
+    }, 3.7).from(texts[1], 1, {
+      autoAlpha: 0,
+      x: '+=40'
+    }, 3.7).to(lines[0], .1, {
+      css: {
+        className: '+=active',
+        animationDuration: '5s'
+      }
+    }, 3).to(bubbles[0], .1, {
+      css: {
+        opacity: 1,
+        className: '+=active',
+        animationDuration: '.5s'
+      }
+    }, 4.4).from(texts[0], 1, {
+      autoAlpha: 0,
+      x: '-=40'
+    }, 4.4);
+    tween.play()
+
+  });
+
+
 
 });
