185edaf8a7eca1853013ddb039c3f069c0f0f8e8
Afshin Moatari <Afshinmoatari@user.noreply.github.com>
2017-11-17 03:23:50 +0100

    Improved cross browser for homepage sections, optimized footer for cross size screens

--- app/js/fullPage.js
+++ app/js/fullPage.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('.fullpage').fullpage({
-    // scrollBar: true,
+    scrollBar: true,
     verticalCentered: true,
     paddingTop: '60px',
     anchors: ['first', 'second', 'third', 'fourth', 'fifth', 'sixth'],
