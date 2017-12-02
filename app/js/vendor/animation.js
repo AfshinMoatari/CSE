84c634304322caac1bacc0ace9979711d08e083c
Afshin Moatari <Afshinmoatari@user.noreply.github.com>
2017-12-02 14:02:45 +0100

    added Concatenate JS, Minified HTML, Uglify CSS and Compress Image gulp tasks.
    updated the animation and layout for landing and collabrations page.

--- app/js/vendor/animation.js
+++ app/js/vendor/animation.js
@@ -1,4 +1,12 @@
 $(function() {
+  if ($('body').is('#main')) {
+    //fullpageJS init
+    $('.fullpage').fullpage({
+      scrollBar: true,
+      verticalCentered: true,
+      paddingTop: '60px',
+    });
+  }
   // init controller
   var controller = new ScrollMagic.Controller();
   // animation classes
@@ -138,7 +146,7 @@ $(function() {
       opacity: 0,
     }, {
       opacity: 1,
-      ease: Power4.easeOut,
+      ease: Expo.easeOut,
       delay: delay
     });
 
@@ -177,22 +185,19 @@ $(function() {
       .addTo(controller);
   });
   $('.square').each(function() {
-
     var duration = $(this).attr('duration-square');
-
     var tween = new TimelineMax();
     $(this).children('span').each(function() {
-
       var maxWidth = $(this).css("max-width");
       var maxHeight = $(this).css("max-height");
-
       if (maxWidth !== 'none') {
         tween.fromTo($(this), duration, {
           autoAlpha: 0,
           width: $(this).css("min-width")
         }, {
           autoAlpha: 1,
-          width: maxWidth
+          width: maxWidth,
+          ease: Back.easeOut,
         })
       } else if (maxHeight !== 'none') {
         tween.fromTo($(this), duration, {
@@ -200,10 +205,10 @@ $(function() {
           height: $(this).css("min-height")
         }, {
           autoAlpha: 1,
-          height: maxHeight
+          height: maxHeight,
+          ease: Back.easeOut,
         })
       }
-
     });
 
     // build a scene
@@ -385,26 +390,6 @@ $(function() {
     // call a hover scene
     $(this).hover(over, out);
   });
-  $('.typewriter').each(function() {
-
-    var duration = $(this).attr('duration');
-    var mySplitText = new SplitText($(this), {
-      type: "words,chars"
-    });
-    var chars = mySplitText.chars;
-    // build a tween
-    var tween = TweenMax.staggerFrom(chars, duration, {
-      opacity: 0,
-      ease: Power1.easeIn
-    }, 0.08);
-
-    // build a scene
-    var scene = new ScrollMagic.Scene({
-        triggerElement: $(this).parents('.animate').first()[0]
-      })
-      .setTween(tween)
-      .addTo(controller);
-  });
   $('.smooth-scroll > *').each(function() {
     var toggler = $(this);
     var menuanchor = toggler.data("menuanchor");
@@ -433,5 +418,84 @@ $(function() {
       }
     });
   });
+  $('#cse-proof-plan').each(function() {
+    var lines = $(this).find('.line');
+    var bubbles = $(this).find('.bubble');
+    var texts = $(this).find('.text');
+    // build a tween
+    tween = new TimelineMax();
+    var tween = tween.to(lines[4], .1, {
+      css: {
+        className: '+=active',
+        animationDuration: '5s'
+      }
+    }, 0).to(bubbles[4], .1, {
+      css: {
+        opacity: 1,
+        className: '+=active',
+        animationDuration: '.5s'
+      }
+    }, 0.5).from(texts[4], 1, {
+      autoAlpha: 0,
+      x: '-=40'
+    }, 0.5).to(lines[3], .1, {
+      css: {
+        className: '+=active',
+        animationDuration: '3.5s'
+      }
+    }, 1).to(bubbles[3], .1, {
+      css: {
+        opacity: 1,
+        className: '+=active',
+        animationDuration: '.5s'
+      }
+    }, 2).from(texts[3], 1, {
+      autoAlpha: 0,
+      x: '+=40'
+    }, 2).to(lines[2], .1, {
+      css: {
+        className: '+=active',
+        animationDuration: '5s'
+      }
+    }, 2).to(bubbles[2], .1, {
+      css: {
+        opacity: 1,
+        className: '+=active',
+        animationDuration: '.5s'
+      }
+    }, 2.7).from(texts[2], 1, {
+      autoAlpha: 0,
+      x: '+=40'
+    }, 2.7).to(lines[1], .1, {
+      css: {
+        className: '+=active',
+        animationDuration: '5s'
+      }
+    }, 3).to(bubbles[1], .1, {
+      css: {
+        opacity: 1,
+        className: '+=active',
+        animationDuration: '.5s'
+      }
+    }, 3.7).from(texts[1], 1, {
+      autoAlpha: 0,
+      x: '+=40'
+    }, 3.7).to(lines[0], .1, {
+      css: {
+        className: '+=active',
+        animationDuration: '5s'
+      }
+    }, 3).to(bubbles[0], .1, {
+      css: {
+        opacity: 1,
+        className: '+=active',
+        animationDuration: '.5s'
+      }
+    }, 4.4).from(texts[0], 1, {
+      autoAlpha: 0,
+      x: '-=40'
+    }, 4.4);
+    tween.play()
 
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,32 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var modernizr = require('gulp-modernizr');
 var sass = require('gulp-sass');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify');
+var order = require('gulp-order');
+var concat = require('gulp-concat');
 var deploy = require('gulp-gh-pages');
 
 // Static Server + watching scss/js/html files
-gulp.task('serve', ['sass', 'js', 'modernizr'], function() {
+gulp.task('serve', ['sass', 'js', 'modernizr', 'imgComp', 'HTMLmini', 'CombineJS'], function() {
 
   browserSync.init({
     server: "./app"
   });
-
+  gulp.watch("app/js/vendor/**/*.js", ['CombineJS']);
+  gulp.watch("app/html-dev/*.html", ['HTMLmini']);
   gulp.watch("app/scss/*.scss", ['sass']);
-  gulp.watch("app/js/*.js", ['js']);
-  gulp.watch("app/*.html").on('change', browserSync.reload);
+  gulp.watch("app/js/vendor/*.js", ['js']);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
+
   return gulp.src("app/scss/*.scss")
-    .pipe(sass())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
     .pipe(gulp.dest("app/css"))
     .pipe(browserSync.stream());
 });
@@ -32,15 +40,45 @@ gulp.task('modernizr', function() {
     .pipe(gulp.dest('node_modules/modernizr/'));
 });
 
+// Compress images
+gulp.task('imgComp', function() {
+  gulp.src('app/assets/image/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('app/assets/image'));
+});
+
+// Minify HTML
+gulp.task('HTMLmini', function() {
+  return gulp.src('app/html-dev/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('app'))
+    .pipe(browserSync.stream());
+});
+
 // Move the javascript files into the app/js folder
 gulp.task('js', function() {
   return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js', 'node_modules/gsap/src/minified/TweenMax.min.js',
       'node_modules/gsap/src/minified/easing/EasePack.min.js', 'node_modules/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js',
       'node_modules/fullpage.js/dist/jquery.fullpage.min.js',
-      'node_modules/fullpage.js/vendors/scrolloverflow.min.js',
-      'node_modules/modernizr/modernizr.js',
-      'node_modules/object-fit-images/dist/ofi.min.js'
+      'node_modules/modernizr/modernizr.js'
     ])
+    .pipe(gulp.dest("app/js/lib"))
+    .pipe(browserSync.stream());
+});
+
+// Combine JS files
+gulp.task('CombineJS', function() {
+  return gulp.src("app/js/vendor/**/*.js")
+    .pipe(order([
+      "animation.js",
+      "media.js"
+    ]))
+    .pipe(concat("app.js"))
+    .pipe(uglify({
+      mangle: false
+    }))
     .pipe(gulp.dest("app/js"))
     .pipe(browserSync.stream());
 });
